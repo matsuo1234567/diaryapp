02b9ea3a55b7cc9ae46e9a93d09d07bcc99a5ae7
Akira_0809 <102137735+Akira0809@users.noreply.github.com>
2023-08-26 09:35:13 +0900

    API完成

--- server/diaryapp/admin.py
+++ server/diaryapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Data
 # Register your models here.
+
+class DataAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Data, DataAdmin)
--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--0r*s_r+jjw%v$@a2%(c2g)%hn68q0i_r+&bvh@@wx6r(%kn9@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diaryapp',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "diaryapp")
+
+MEDIA_URL = "/server/img/"
